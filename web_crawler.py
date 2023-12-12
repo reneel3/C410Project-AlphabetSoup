ecce5cb8ec4165ce69017c3d080d776e9619f66c
Shihab00 <66040531+Shihab00@users.noreply.github.com>
2023-12-11 19:27:43 -0500

    made web crawler function <CREPE>

--- web_crawler.py
+++ web_crawler.py
@@ -1,17 +1,24 @@
 from googlesearch import search
 from zenrows import ZenRowsClient
 
-query = "Pumpkin Pie Recipes"
+#query = "Pumpkin Pie Recipes"
 
-scrape_urls = []
-for url in search(query, stop=10):
-    scrape_urls.append(url)
-print(scrape_urls)
+def crepes(query): 
+    scrape_urls = []
+    for url in search(query, stop=10):
+        scrape_urls.append(url)
+    #print(scrape_urls)
 
-results = []
-for url in scrape_urls:
-    client = ZenRowsClient("b7b02bdb545da1de88ffffb5b1c12bbd5a3607fc")
-    params = {"css_extractor" : "{\"title\":\"h1\", \"recipe\":\"p\"}"}
-    response = client.get(url,params=params).text
-    results.append(response)
-print (results)
+    resps = []
+    for url in scrape_urls:
+        client = ZenRowsClient("b7b02bdb545da1de88ffffb5b1c12bbd5a3607fc")
+        params = {"css_extractor" : "{\"title\":\"h1\", \"recipe\":\"p\"}"}
+        response = client.get(url,params=params).text
+        resps.append(response)
+    #print(resps)
+    
+    results = {}
+    for i in range(len(scrape_urls)):
+        results[scrape_urls[i]] = resps[i]
+
+    return results
