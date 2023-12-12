17901c3481b47caa5758462fb13eb5df04b9c380
Shihab00 <66040531+Shihab00@users.noreply.github.com>
2023-12-11 20:04:17 -0500

    added num, fixed return dictionary

--- web_crawler.py
+++ web_crawler.py
@@ -1,19 +1,21 @@
 from googlesearch import search
 from zenrows import ZenRowsClient
+import json
 
 #query = "Pumpkin Pie Recipes"
 
-def crepes(query): 
+def crepes(query, num): 
     scrape_urls = []
-    for url in search(query, stop=10):
+    for url in search(query, stop=num):
         scrape_urls.append(url)
     #print(scrape_urls)
 
     resps = []
     for url in scrape_urls:
         client = ZenRowsClient("b7b02bdb545da1de88ffffb5b1c12bbd5a3607fc")
-        params = {"css_extractor" : "{\"title\":\"h1\", \"recipe\":\"p\"}"}
-        response = client.get(url,params=params).text
+        params = {"css_extractor" : "{\"title\":\"h1\", \"recipe\":\"p\"}" }
+        response = json.loads(client.get(url,params=params).text)
+        response['recipe'] = text = " ".join(response['recipe'])
         resps.append(response)
     #print(resps)
     
