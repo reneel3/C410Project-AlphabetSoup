5d4e85068ffe1c9473d3fd6360a60422605185df
aashra5 <aashra5@illinois.edu>
2023-12-13 20:25:48 -0500

    adding ranking function

--- web_crawler.py
+++ web_crawler.py
@@ -1,6 +1,8 @@
 from googlesearch import search
 from zenrows import ZenRowsClient
 import json
+import string
+from rank_bm25 import BM25Okapi
 
 #query = "Pumpkin Pie Recipes"
 
@@ -24,3 +26,39 @@ def crepes(query, num):
         results[scrape_urls[i]] = resps[i]
 
     return results
+
+
+
+
+def rankData(query, num):
+
+    doc = crepes(query, num)
+    bmScores = {}
+
+    for key in doc:
+        print("key", key)
+        newdoc = doc[key]['recipe']
+        print("newdoc", newdoc)
+
+        corpus = [newdoc.translate(str.maketrans('', '', string.punctuation)).replace('\n',"").lower()]
+        print("corpus: ", corpus)
+
+        tokenized_corpus = [doc.split(" ") for doc in corpus]
+        print("tokenized_corpus: ", tokenized_corpus)
+
+        bm25 = BM25Okapi(tokenized_corpus)
+        tokenized_query = query.lower().split(" ")
+
+        doc_scores = bm25.get_scores(tokenized_query)
+        print(doc_scores)
+
+        bmScores[key] = abs(float(doc_scores))
+        finalBM = sorted(bmScores.items(), key=lambda x:x[1], reverse=True)
+
+        print("bmScores: ", finalBM)
+        return bmScores
+
+        #bm25.get_top_n(tokenized_query, corpus, n=num)
+
+
+rankData("pumpkin pie recipes", 10)
